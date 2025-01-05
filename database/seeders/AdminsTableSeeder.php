10b118379486347b1ceba332979375b7eabceb7f
cristianaquino <ycristian10@gmail.com>
2025-01-04 21:23:49 -0500

    feat: admin migration and seeder

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        // vaciamos la tabla
+        Admin::truncate();
+
+        $password = Hash::make('12345678');
+
+        // create admin
+        $admin = Admin::create([
+            'code_admin' => 'AD' . (int) date("Y") * 10000,
+            'role' => 'ROLE_SUPERADMIN'
+        ]);
+
+        $admin->user()->create([
+            'name' => 'administrator',
+            'email' => 'admin@admin.com',
+            'password' => $password,
+        ]);
+    }
+}
--- database/seeders/TeachersTableSeeder.php
+++ database/seeders/TeachersTableSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\Course;
 use App\Models\Teacher;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+
 
 class TeachersTableSeeder extends Seeder
 {
@@ -14,12 +16,31 @@ class TeachersTableSeeder extends Seeder
     public function run(): void
     {
         //
-        $courses = Course::all();
+        // vaciamos la tabla
+        Teacher::truncate();
+        $faker = Faker::create();
 
-        foreach ($courses as $course) {
-            $teacher = Teacher::inRandomOrder()->first();
-            $course->teacher_id = $teacher->id;
-            $course->save();
+        $password = Hash::make('12345678');
+
+        // create teacher
+        for ($i = 0; $i < 15; $i++) {
+            # code...
+            $code = (int) date("Y") * 10000 + $i;
+            $teacher = Teacher::create([
+                'code_teacher' => 'TE' . $code,
+                'role' => 'ROLE_TEACHER'
+            ]);
+            $teacher->user()->create([
+                'name' => $faker->name,
+                'first_name' => $faker->firstName,
+                'second_name' => $faker->lastName,
+                'birth_date' => $faker->date('Y-m-d', '1990-12-31'),
+                'address' => $faker->address,
+                'phone' => $faker->phoneNumber,
+                'dni' => (string)$faker->randomNumber(8),
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
         }
     }
 }
