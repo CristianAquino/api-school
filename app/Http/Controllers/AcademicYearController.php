20f0184c82311516aed272005ef055225e1338e0
cristianaquino <ycristian10@gmail.com>
2025-01-24 17:57:16 -0500

    feat: add soft delete to table academic year

--- app/Http/Controllers/AcademicYearController.php
+++ app/Http/Controllers/AcademicYearController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\DTOs\AcademicYearDTO;
 use App\Models\AcademicYear;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class AcademicYearController extends Controller
@@ -21,13 +22,42 @@ class AcademicYearController extends Controller
     }
 
     /**
+     * Display a listing of the resource remove soft.
+     */
+    public function soft_list()
+    {
+        //
+        $response = Gate::inspect('soft_view', AcademicYear::class);
+
+        if (!$response->allowed()) {
+            return response()->json([
+                "message" => $response->message()
+            ], Response::HTTP_FORBIDDEN);
+        }
+
+        $deletedAcademicYears = AcademicYear::onlyTrashed()->get();
+        $deletedAcademicYearsDTO = AcademicYearDTO::fromCollection($deletedAcademicYears);
+        return response()->json($deletedAcademicYearsDTO, Response::HTTP_OK);
+    }
+
+    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
         //
+        $response = Gate::inspect('create', AcademicYear::class);
+
+        if (!$response->allowed()) {
+            return response()->json([
+                "message" => $response->message()
+            ], Response::HTTP_FORBIDDEN);
+        }
+
         AcademicYear::create($request->validated_data);
-        return response()->json(["message" => "The academic year $request->year has been successfully created"], Response::HTTP_CREATED);
+        return response()->json([
+            "message" => "The academic year $request->year has been successfully created"
+        ], Response::HTTP_CREATED);
     }
 
     /**
@@ -40,6 +70,9 @@ class AcademicYearController extends Controller
         return response()->json($academicYearDTO, Response::HTTP_OK);
     }
 
+    /**
+     * Display the last year resource.
+     */
     public function lastYear()
     {
         $lastYear = AcademicYear::latest('year')->first();
@@ -53,28 +86,106 @@ class AcademicYearController extends Controller
     public function update(Request $request, AcademicYear $academicYear)
     {
         //
+        $response = Gate::inspect('update', AcademicYear::class);
+
+        if (!$response->allowed()) {
+            return response()->json([
+                "message" => $response->message()
+            ], Response::HTTP_FORBIDDEN);
+        }
+
         $year = $academicYear->year;
 
         $academicYear->update($request->validated_data);
 
         if ($academicYear->year == $year) {
-            return response()->json(["message" => "the academic year $year has been successfully updated"], Response::HTTP_ACCEPTED);
+            return response()->json([
+                "message" => "the academic year $year has been successfully updated"
+            ], Response::HTTP_ACCEPTED);
         } else {
-            return response()->json(["message" => "the academic year $year has been successfully updated to $academicYear->year"], Response::HTTP_ACCEPTED);
+            return response()->json([
+                "message" => "the academic year $year has been successfully updated to $academicYear->year"
+            ], Response::HTTP_ACCEPTED);
         }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove soft the specified resource from storage.
      */
-    public function destroy(AcademicYear $academicYear)
+    public function soft_destroy(AcademicYear $academicYear)
     {
         //
-        if ($academicYear->enrollements->count() > 0) {
-            return response()->json(["message" => "the academic year $academicYear->year cannot be deleted because it has enrollements"], Response::HTTP_BAD_REQUEST);
+        $response = Gate::inspect('soft_delete', AcademicYear::class);
+
+        if (!$response->allowed()) {
+            return response()->json([
+                "message" => $response->message()
+            ], Response::HTTP_FORBIDDEN);
         }
 
         $academicYear->delete();
-        return response()->json(["message" => "the academic year $academicYear->year has been successfully deleted"], Response::HTTP_ACCEPTED);
+        return response()->json([
+            "message" => "the academic year $academicYear->year has been successfully deleted"
+        ], Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * Restore the specified resource from storage.
+     */
+    public function restore($id)
+    {
+        //
+        $response = Gate::inspect('restore', AcademicYear::class);
+
+        if (!$response->allowed()) {
+            return response()->json([
+                "message" => $response->message()
+            ], Response::HTTP_FORBIDDEN);
+        }
+
+        $academicYear = AcademicYear::onlyTrashed()->find($id);
+
+        if (is_null($academicYear)) {
+            return response()->json([
+                "message" => "the academic year does not exist"
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $academicYear->restore();
+        return response()->json([
+            "message" => "the academic year $academicYear->year has been successfully restored"
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove permanently the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        //
+        $response = Gate::inspect('forceDelete', AcademicYear::class);
+
+        if (!$response->allowed()) {
+            return response()->json([
+                "message" => $response->message()
+            ], Response::HTTP_FORBIDDEN);
+        }
+        // if ($academicYear->enrollements->count() > 0) {
+        //     return response()->json([
+        //         "message" => "the academic year $academicYear->year cannot be deleted because it has enrollements"
+        //     ], Response::HTTP_BAD_REQUEST);
+        // }
+        $academicYear = AcademicYear::onlyTrashed()->find($id);
+
+        if (is_null($academicYear)) {
+            return response()->json([
+                "message" => "the academic year does not exist"
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $academicYear->forceDelete();
+        return response()->json([
+            "message" => "the academic year $academicYear->year has been successfully deleted permanently"
+        ], Response::HTTP_ACCEPTED);
     }
 }
--- database/migrations/2024_12_26_025050_create_academic_years_table.php
+++ database/migrations/2024_12_26_025050_create_academic_years_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('year', 4)->unique()->default(date("Y"));
             $table->date('start_date')->default(date("Y/m/d"));
             $table->date('end_date')->default(date("Y/12/31"));
+            $table->softDeletes();
             $table->timestamps();
         });
     }
