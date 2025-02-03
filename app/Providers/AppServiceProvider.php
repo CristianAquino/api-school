54d19709476d900501e93a41ae984b45d44e3e31
cristianaquino <ycristian10@gmail.com>
2025-02-03 10:19:52 -0500

    feat: register polices

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\AcademicYear;
+use App\Models\Course;
+use App\Models\Enrollement;
+use App\Models\Grade;
+use App\Models\Level;
+use App\Models\Schedule;
+use App\Models\Teacher;
+use App\Policies\GeneralPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +29,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        // registrando una politica general para cada modelo
+        Gate::policy(AcademicYear::class, GeneralPolicy::class);
+        Gate::policy(Level::class, GeneralPolicy::class);
+        Gate::policy(Grade::class, GeneralPolicy::class);
+        Gate::policy(Course::class, GeneralPolicy::class);
+        Gate::policy(Schedule::class, GeneralPolicy::class);
+        Gate::policy(Teacher::class, GeneralPolicy::class);
+        Gate::policy(Enrollement::class, GeneralPolicy::class);
     }
 }
